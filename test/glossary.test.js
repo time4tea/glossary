c31c4314d1ddfe7b04fc2fc1ee7da449fb56c4a8
James Richardson <james@time4tea.net>
2014-11-17 23:14:22 +0000

    phrases with metacharacters

--- test/glossary.test.js
+++ test/glossary.test.js
@@ -21,44 +21,55 @@ describe("a simple glossary", function () {
   g.add("item", ["user", "stuff"]);
 
   it("will find words", function () {
-    assert.equal(1, numberOfFoundItemsIn(g.gloss("this is a string with item bob")), "should find 1 item");
-    assert.equal(3, numberOfFoundItemsIn(g.gloss("item item item")), "should find 3 items");
+    assert.equal(numberOfFoundItemsIn(g.gloss("this is a string with item bob")), 1);
+    assert.equal(numberOfFoundItemsIn(g.gloss("item item item")), 3);
   });
 
   it("only finds whole words", function () {
-    assert.equal(3, numberOfFoundItemsIn(g.gloss("item item item itemitem")));
+    assert.equal(numberOfFoundItemsIn(g.gloss("item item item itemitem")), 3);
   });
 
   it("finds words with punctuation around", function () {
-    assert.equal(2, numberOfFoundItemsIn(g.gloss(".item.item")));
-    assert.equal(2, numberOfFoundItemsIn(g.gloss(".item.(item)")));
+    assert.equal(numberOfFoundItemsIn(g.gloss(".item.item")), 2);
+    assert.equal(numberOfFoundItemsIn(g.gloss(".item.(item)")), 2);
   });
 });
 
-describe("glossary with multiple words", function() {
+describe("glossary with multiple words", function () {
   var g = new Glossary();
   g.add("item1", []);
   g.add("item2", []);
 
   it("will find words", function () {
-    assert.equal(1, numberOfFoundItemsIn(g.gloss("this is a string with item1")));
-    assert.equal(1, numberOfFoundItemsIn(g.gloss("this is a string with item2")));
-    assert.equal(2, numberOfFoundItemsIn(g.gloss("this is a string with item1 and item2")));
-    assert.equal(0, numberOfFoundItemsIn(g.gloss("item item item")));
-    assert.equal(4, numberOfFoundItemsIn(g.gloss("item1 item2 item1 item2 something else")));
+    assert.equal(numberOfFoundItemsIn(g.gloss("this is a string with item1")), 1);
+    assert.equal(numberOfFoundItemsIn(g.gloss("this is a string with item2")), 1);
+    assert.equal(numberOfFoundItemsIn(g.gloss("this is a string with item1 and item2")), 2);
+    assert.equal(numberOfFoundItemsIn(g.gloss("item item item")), 0);
+    assert.equal(numberOfFoundItemsIn(g.gloss("item1 item2 item1 item2 something else")), 4);
   });
 });
 
-describe("glossary with phrases", function() {
+describe("glossary with phrases", function () {
   var g = new Glossary();
   g.add("something nice", []);
   g.add("item2", []);
 
   it("will find words", function () {
-    assert.equal(1, numberOfFoundItemsIn(g.gloss("this is a string with something nice")));
-    assert.equal(1, numberOfFoundItemsIn(g.gloss("this is a string with item2")));
-    assert.equal(0, numberOfFoundItemsIn(g.gloss("something item nice")));
-    assert.equal(0, numberOfFoundItemsIn(g.gloss("something  nice")));
+    assert.equal(numberOfFoundItemsIn(g.gloss("this is a string with something nice")), 1);
+    assert.equal(numberOfFoundItemsIn(g.gloss("this is a string with item2")), 1);
+    assert.equal(numberOfFoundItemsIn(g.gloss("something item nice")), 0);
+    assert.equal(numberOfFoundItemsIn(g.gloss("something  nice")), 0);
+  });
+});
+
+describe("phrases with punctuation", function () {
+  var g = new Glossary();
+  g.add("j.r. hartley", []);
+
+  it("will find words", function () {
+    assert.equal(numberOfFoundItemsIn(g.gloss("my name you see, is j.r. hartley")), 1);
+    // . in a regex is a wildcard
+    assert.equal(numberOfFoundItemsIn(g.gloss("my name you see, is jxrx hartley")), 0);
   });
 });
 
@@ -78,14 +89,14 @@ describe("tree traversal", function () {
       }
     });
 
-    assert.equal("x x x x, bobitty x bobitty, x x x x", str);
+    assert.equal(str, "x x x x, bobitty x bobitty, x x x x");
   });
 
 
   it("calls us back with user data", function () {
     g.gloss("bob").visit({
       gloss: function (text, ud) {
-        assert.equal("stuff", ud[1]);
+        assert.equal(ud[1], "stuff");
       }
     });
   });
